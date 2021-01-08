567941e2844a61b15278d4e55201948dd56762f0
fahminovianto <76575109+fahminov@users.noreply.github.com>
2021-01-08 11:11:28 +0700

    Update HandleMsg.js

--- HandleMsg.js
+++ HandleMsg.js
@@ -2363,7 +2363,7 @@ _Desc di update oleh : @${chat.groupMetadata.descOwner.replace('@c.us','')} pada
 
                     break
                     case 'grupbot':
-                        const ch = `https://chat.whatsapp.com/Lt96VeJbmMHFeB1QDDGtPM\n\nSkuy join grup Bot niscaya mendapatkan Doi`
+                        const ch = `https://chat.whatsapp.com/KiNhPRdrYVY0KWKkSqiRiJ\n\nSkuy join grup Bot niscaya mendapatkan Doi`
                         await aruga.sendText(from, ch, id)
                         break
                     case 'mtk':
